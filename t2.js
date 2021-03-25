c3872637fbfac829ae57ecc87f4c7daf71ac5022
cordis-dev <darius@adroiti.com>
2021-03-25 12:28:09 +0200

    Update t2.js

--- t2.js
+++ t2.js
@@ -1,9 +1,5 @@
 import { cleanup } from '@testing-library/react';
-
-const { cleanup } = require('@testing-library/react');
-
 import utils from '@testing-library/react';
-afterEach(() => utils.cleanup());
 
-const utils = require('@testing-library/react');
+afterEach(() => utils.cleanup());
 afterEach(utils.cleanup);
